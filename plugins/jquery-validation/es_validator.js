6603177551bc66783313715e6791d9c4451e4b83
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2013-07-25 13:42:06 -0500

    Validador/ Add Rule message

--- plugins/jquery-validation/es_validator.js
+++ plugins/jquery-validation/es_validator.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 	  email: 'Correo invalido',
 	  equalTo: 'Ingresa los mismos valores.',
 	  number : 'Este campo tiene que ser un valor númerico',
-	  minlength: 'Este campo tiene que tener minimo 8 caracteres'
+	  minlength: 'Este campo tiene que tener minimo 8 caracteres',
+	  max: 'Los campos deben de sumar un total de 100%'
 	  
 	});
     
