05d60089001b57292157df4a7cafbaf251851ff5
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2013-08-17 13:30:19 -0500

    Work Order/330

--- application/modules/ot/views/list.php
+++ application/modules/ot/views/list.php
@@ -103,12 +103,12 @@
                       <div class="row advanced">
                       		
                             <a href="javascript:void(0);" class="btn btn-link findsub" id="activadas">Activadas</a>
-                            <a href="javascript:void(0);" class="btn btn-link findsub" id="tramite">En trámite</a>
+                            <a href="javascript:void(0);" class="btn btn-primary findsub" id="tramite">En trámite</a>
                             <a href="javascript:void(0);" class="btn btn-link findsub" id="terminada"> Terminadas</a>
                             <a href="javascript:void(0);" class="btn btn-link findsub" id="canceladas">Canceladas</a>
                           <!--  <a href="javascript:void(0);" class="btn btn-link findsub" id="pagadas">Pagadas</a>
                             <a href="javascript:void(0);" class="btn btn-link findsub" id="excedido">Excedido</a>-->
-                            <a href="javascript:void(0);" class="btn btn-primary findsub" id="todas">Todas</a>
+                            <a href="javascript:void(0);" class="btn btn-link findsub" id="todas">Todas</a>
                             <br /><br />
                              <table class="table table-bordered bootstrap-datatable datatable">           	
                              	<tr>
