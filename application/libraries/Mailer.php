9dc38dad96e090aaff397e34c22e15101f26c5ce
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2013-08-20 13:25:47 -0500

    mailer

--- application/libraries/Mailer.php
+++ application/libraries/Mailer.php
@@ -233,9 +233,7 @@ class Mailer{
 			
 						
 			@mail( $value['email'],  $status_name. ' de la Orden de Trabajo '.$notification[0]['uid'], $body, $headers );
-			
-			echo $body;
-			exit;			
+					
 			
 			/*
 						
