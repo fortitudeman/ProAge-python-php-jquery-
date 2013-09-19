262fbf348fc18e4cfe4e02c19892720b4721fda7
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2013-09-19 02:27:19 -0500

    Report/update

--- application/modules/ot/views/report.php
+++ application/modules/ot/views/report.php
@@ -140,7 +140,7 @@
                                     <th>&nbsp; </th>
                                     <th>&nbsp; </th>
                                     <th>&nbsp; </th>
-                                    <th>&nbsp; </th>
+                                    <th><input type="submit" value="Filtrar" class="btn btn-inverse" /></th>
                                     <th width="50%" align="right" > <a href="#">
                                         <img src="<?php echo base_url() ?>ot/assets/images/down.png"></a></th>
                                 </tr>
