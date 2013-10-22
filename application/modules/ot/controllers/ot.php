3b454d4bd9356d055231f600483503047951db72
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2013-10-21 22:51:08 -0500

    Import Payments /debug

--- application/modules/ot/controllers/ot.php
+++ application/modules/ot/controllers/ot.php
@@ -1877,10 +1877,12 @@ class Ot extends CI_Controller {
 		  
 		  $file_array = $this->work_order->getImportPaymentsTmp();
 		  
+		   print_r( $file_array );
+		  exit;	
+		  
 		  $file_array = json_decode( $file_array[0]['data'] );
 		  
-		  print_r( $file_array );
-		  exit;	
+		 
 		  
 		  
 		  $tmp_file = $_POST['tmp_file'];
