6307984cc7d0ff1987d0729c2018f0c970c88f01
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2013-12-19 22:46:15 -0600

    Metas / Format number solicitud meta

--- application/modules/simulator/assets/scripts/metas.js
+++ application/modules/simulator/assets/scripts/metas.js
@@ -324,7 +324,7 @@ function getMetas(){
 			$( '#primas-negocios-meta-text-'+i ).html( Math.round(primapromedio) );			
 			// Solicitud Meta
 			$( '#primas-solicitud-meta-'+i ).val( Math.round(solicitud) );
-			$( '#primas-solicitud-meta-text-'+i ).html( Math.round(solicitud) );	
+			$( '#primas-solicitud-meta-text-'+i ).html( '$ ' + moneyFormat(Math.round(solicitud)));	
 			if( !isNaN( meta ) )  totaltrimestre+=meta;
 			if( !isNaN( meta ) ) totalprimameta+=meta;
 			if( !isNaN( primapromedio ) )  totalnegociometa += primapromedio;
@@ -339,7 +339,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-primer' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-primer-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-primer' ).val( Math.round(totalsolicitudmeta) );
-				$( '#primas-solicitud-meta-primer-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-primer-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;				
@@ -350,7 +350,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-segund' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-segund-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-segund' ).val( Math.round(totalsolicitudmeta) );
-				$( '#primas-solicitud-meta-segund-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-segund-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;				
@@ -361,7 +361,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-tercer' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-tercer-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-tercer' ).val( Math.round(totalsolicitudmeta) );
-				$( '#primas-solicitud-meta-tercer-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-tercer-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;
@@ -372,7 +372,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-cuarto' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-cuarto-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-cuarto' ).val( Math.round(totalsolicitudmeta) );
-				$( '#primas-solicitud-meta-cuarto-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-cuarto-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;				
@@ -384,7 +384,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-primer' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-primer-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-primer' ).val( totalsolicitudmeta );
-				$( '#primas-solicitud-meta-primer-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-primer-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;				
@@ -395,7 +395,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-second' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-second-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-second' ).val( totalsolicitudmeta );
-				$( '#primas-solicitud-meta-second-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-second-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;				
@@ -406,7 +406,7 @@ function getMetas(){
 				$( '#primas-negocio-meta-tercer' ).val( Math.round(totalnegociometa) );
 				$( '#primas-negocio-meta-tercer-text' ).html( Math.round(totalnegociometa) );				
 				$( '#primas-solicitud-meta-tercer' ).val( totalsolicitudmeta );
-				$( '#primas-solicitud-meta-tercer-text' ).html( Math.round(totalsolicitudmeta) );				
+				$( '#primas-solicitud-meta-tercer-text' ).html( '$ ' + moneyFormat(Math.round(totalsolicitudmeta)) );				
 				totaltrimestre = 0;				
 				totalnegociometa= 0;				
 				totalsolicitudmeta= 0;				
@@ -419,7 +419,7 @@ function getMetas(){
 		$( '#primas-negocios-meta-total-text' ).html( Math.round(totalesnegociometa) );
 		
 		$( '#primas-solicitud-meta-total' ).val( Math.round(totalessolicitudmeta) );
-		$( '#primas-solicitud-meta-total-text' ).html( Math.round(totalessolicitudmeta) );
+		$( '#primas-solicitud-meta-total-text' ).html( '$ ' + moneyFormat(Math.round(totalessolicitudmeta)) );
 		
 }
 function moneyFormat( n ){	
