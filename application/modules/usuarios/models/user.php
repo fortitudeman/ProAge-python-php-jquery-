c497cf9dd885298d24f2c73b40def31c56c3404f
flai <flai1789@gmail.com>
2014-05-22 11:35:52 +0200

    ot/reporte.html: take the period filter into account only to compute columns "Negocios pagados", "Negocios PAI" and "Primas pagadas"

--- application/modules/usuarios/models/user.php
+++ application/modules/usuarios/models/user.php
@@ -3038,51 +3038,6 @@ AND
 				$ramo = $filter['query']['ramo'];
 			}
 
-			/*
-			<option value="1">Mes</option>
-			<option value="2">Trimestre (Vida) o cuatrimestre (GMM)</option>
-			<option value="3">Año</option>
-			*/	
-
-			if( isset( $filter['query']['periodo'] ) and !empty( $filter['query']['periodo'])) {
-
-				$period = $filter['query']['periodo'];
-				if( $filter['query']['periodo'] == 1 )			
-					$this->db->where( 'work_order.creation_date >= ', date( 'Y' ).'-'.(date( 'm' )).'-01'); 
-
-				if( $filter['query']['periodo'] == 2 )
-				{
-					$this->load->helper('tri_cuatrimester');
-					if( isset( $filter['query']['ramo'] ) and $filter['query']['ramo'] == 2 or $filter['query']['ramo'] == 3 )
-						$begin_end = get_tri_cuatrimester( $this->cuatrimestre(), 'cuatrimestre' ) ;
-					else
-						$begin_end = get_tri_cuatrimester( $this->trimestre(), 'trimestre' );
-
-					if (isset($begin_end) && isset($begin_end['begind']) && isset($begin_end['end']))
-						$this->db->where( array(
-							'work_order.creation_date >= ' => $begin_end['begind'],
-							'work_order.creation_date <=' =>  $begin_end['end']) );
-				}
-				if( $filter['query']['periodo'] == 3 )
-				{
-					$this->db->where( array(
-						'work_order.creation_date >= ' => date( 'Y' ).'-01-01',
-						'work_order.creation_date <=' => date( 'Y-m-d' ) .  ' 23:59:59' ) ); 
-				}
-				if( $filter['query']['periodo'] == 4 )
-				{
-					$from = $this->custom_period_from;
-					$to = $this->custom_period_to;
-					if ( ( $from === FALSE ) || ( $to === FALSE ) )
-					{
-						$from = date('Y-m-d');
-						$to = $from;
-					}
-					$this->db->where( array(
-						'work_order.creation_date >= ' => $from . ' 00:00:00',
-						'work_order.creation_date <=' => $to . ' 23:59:59') );
-				}
-			}
 		}
 		$query = $this->db->get();
 
@@ -3162,51 +3117,6 @@ AND
 				$this->db->where( 'work_order.product_group_id', $filter['query']['ramo'] );
 				$ramo = $filter['query']['ramo'];
 			}
-
-			/*
-			<option value="1">Mes</option>
-			<option value="2">Trimestre (Vida) o cuatrimestre (GMM)</option>
-			<option value="3">Año</option>
-			*/	
-			if( isset( $filter['query']['periodo'] ) and !empty( $filter['query']['periodo'])) {
-
-				$period = $filter['query']['periodo'];
-				if( $filter['query']['periodo'] == 1 )			
-					$this->db->where( 'work_order.creation_date >= ', date( 'Y' ).'-'.(date( 'm' )).'-01'); 
-
-				if( $filter['query']['periodo'] == 2 )
-				{
-					$this->load->helper('tri_cuatrimester');
-					if( isset( $filter['query']['ramo'] ) and $filter['query']['ramo'] == 2 or $filter['query']['ramo'] == 3 )
-						$begin_end = get_tri_cuatrimester( $this->cuatrimestre(), 'cuatrimestre' ) ;
-					else
-						$begin_end = get_tri_cuatrimester( $this->trimestre(), 'trimestre' );
-
-					if (isset($begin_end) && isset($begin_end['begind']) && isset($begin_end['end']))
-						$this->db->where( array(
-							'work_order.creation_date >= ' => $begin_end['begind'],
-							'work_order.creation_date <=' =>  $begin_end['end']) );
-				}
-				if( $filter['query']['periodo'] == 3 )
-				{
-					$this->db->where( array(
-						'work_order.creation_date >= ' => date( 'Y' ).'-01-01',
-						'work_order.creation_date <=' => date( 'Y-m-d' ) .  ' 23:59:59' ) ); 
-				}
-				if( $filter['query']['periodo'] == 4 )
-				{
-					$from = $this->custom_period_from;
-					$to = $this->custom_period_to;
-					if ( ( $from === FALSE ) || ( $to === FALSE ) )
-					{
-						$from = date('Y-m-d');
-						$to = $from;
-					}
-					$this->db->where( array(
-						'work_order.creation_date >= ' => $from . ' 00:00:00',
-						'work_order.creation_date <=' => $to . ' 23:59:59') );
-				}
-			}
 		}
 
 		$query = $this->db->get(); 
@@ -3264,115 +3174,16 @@ AND
 	$this->db->join( 'users', 'users.id=agents.user_id' );
 	$this->db->where( 'policies_vs_users.user_id', $user_id );
 	
-if( !empty( $filter ) ){
-			
-			
+	if( !empty( $filter ) ){
+	
 		if( isset( $filter['query']['ramo'] ) and !empty( $filter['query']['ramo'] ) ){
 					
 			$this->db->where( 'work_order.product_group_id', $filter['query']['ramo'] ); 
 		}
-		
-		/*
-		<option value="1">Mes</option>
-		<option value="2">Trimestre (Vida) o cuatrimestre (GMM)</option>
-		<option value="3">Año</option>
-		*/	
-		
-		$mes = date( 'Y' ).'-'.(date( 'm' )).'-01';
-		
-		
-		$trimestre = $this->trimestre();
-		
-		$cuatrimetre = $this->cuatrimestre();
-								
-		$anio = date( 'Y' ).'-01-01';
-					
-	if( isset( $filter['query']['periodo'] ) and !empty( $filter['query']['periodo'] ) ){
-		
-		
-		if( $filter['query']['periodo'] == 1 )
-		
-			$this->db->where( 'work_order.creation_date >= ', $mes); 
-		
-		
-		
-		if( $filter['query']['periodo'] == 2 )
-		
-			
-			if( isset( $filter['query']['ramo'] ) and $filter['query']['ramo'] == 2 or $filter['query']['ramo'] == 3 ){
-				
-				if( $cuatrimetre == 1 ){			
-					$begind = date( 'Y' ).'-01-01';	
-					$end = date( 'Y' ).'-04-'.date('d');	
-				}
-				
-				if( $cuatrimetre == 2 ){			
-					$begind = date( 'Y' ).'-04-01';	
-					$end = date( 'Y' ).'-08-'.date('d');	
-				}
-				
-				if( $cuatrimetre == 3 ){			
-					$begind = date( 'Y' ).'-08-01';	
-					$end = date( 'Y' ).'-12-'.date('d');	
-				}
-				
-				$this->db->where( array( 'work_order.creation_date >= ' =>  $begind , 'work_order.creation_date <=' =>  $end  ) ); 
-			
-			}else{
-				
-				
-				if( $trimestre == 1 ){			
-					$begind = date( 'Y' ).'-01-01';	
-					$end = date( 'Y' ).'-03-'.date('d');	
-				}
-				
-				if( $trimestre == 2 ){			
-					$begind = date( 'Y' ).'-03-01';	
-					$end = date( 'Y' ).'-06-'.date('d');	
-				}
-				
-				if( $trimestre == 3 ){			
-					$begind = date( 'Y' ).'-06-01';	
-					$end = date( 'Y' ).'-09-'.date('d');	
-				}
-				
-				if( $trimestre == 4 ){			
-					$begind = date( 'Y' ).'-09-01';	
-					$end = date( 'Y' ).'-12-'.date('d');	
-				}
-				
-					
-				$this->db->where( array( 'work_order.creation_date >= ' => $begind, 'work_order.creation_date <=' =>  $end ) ); 
-			}
-			
-			
-			
-			
-			
-			
-		if( $filter['query']['periodo'] == 3 )
-		
-			$this->db->where( array( 'work_order.creation_date >= ' => $anio,  'work_order.creation_date <=' => date( 'Y-m-d' ) ) ); 
+	}
 
-		if( $filter['query']['periodo'] == 4 )
-		{
-			$from = $this->custom_period_from;
-			$to = $this->custom_period_to;
-			if ( ( $from === FALSE ) || ( $to === FALSE ) )
-			{
-				$from = date('Y-m-d');
-				$to = $from;
-			}
-			$this->db->where( array(
-				'work_order.creation_date >= ' => $from . ' 00:00:00',
-				'work_order.creation_date <=' => $to . ' 23:59:59') );
-		}
-	}				
-}
-	
-	
 	$query = $this->db->get(); 
-  	
+	
 	if ($query->num_rows() == 0) return 0;	
 	
 	$count = 0;
@@ -3431,113 +3242,14 @@ if( !empty( $filter ) ){
 	$this->db->where( 'policies_vs_users.user_id', $user_id );
 	
 	if( !empty( $filter ) ){
-			
-			
-	  if( isset( $filter['query']['ramo'] ) and !empty( $filter['query']['ramo'] ) ){
-				  
-		  $this->db->where( 'work_order.product_group_id', $filter['query']['ramo'] ); 
-	  }
-	  
-	  /*
-	  <option value="1">Mes</option>
-	  <option value="2">Trimestre (Vida) o cuatrimestre (GMM)</option>
-	  <option value="3">Año</option>
-	  */	
-	  
-	  $mes = date( 'Y' ).'-'.(date( 'm' )).'-01';
-	  
-	  
-	  $trimestre = $this->trimestre();
-	  
-	  $cuatrimetre = $this->cuatrimestre();
-							  
-	  $anio = date( 'Y' ).'-01-01';
-				  
-	  if( isset( $filter['query']['periodo'] ) and !empty( $filter['query']['periodo'] ) ){
-	  
-	  
-	  if( $filter['query']['periodo'] == 1 )
-	  
-		  $this->db->where( 'work_order.creation_date >= ', $mes); 
-	  
-	  
-	  
-	  if( $filter['query']['periodo'] == 2 )
-	  
-		  
-		  if( isset( $filter['query']['ramo'] ) and $filter['query']['ramo'] == 2 or $filter['query']['ramo'] == 3 ){
-			  
-			  if( $cuatrimetre == 1 ){			
-				  $begind = date( 'Y' ).'-01-01';	
-				  $end = date( 'Y' ).'-04-'.date('d');	
-			  }
-			  
-			  if( $cuatrimetre == 2 ){			
-				  $begind = date( 'Y' ).'-04-01';	
-				  $end = date( 'Y' ).'-08-'.date('d');	
-			  }
-			  
-			  if( $cuatrimetre == 3 ){			
-				  $begind = date( 'Y' ).'-08-01';	
-				  $end = date( 'Y' ).'-12-'.date('d');	
-			  }
-			  
-			  $this->db->where( array( 'work_order.creation_date >= ' =>  $begind , 'work_order.creation_date <=' =>  $end  ) ); 
-		  
-		  }else{
-			  
-			  
-			  if( $trimestre == 1 ){			
-				  $begind = date( 'Y' ).'-01-01';	
-				  $end = date( 'Y' ).'-03-'.date('d');	
-			  }
-			  
-			  if( $trimestre == 2 ){			
-				  $begind = date( 'Y' ).'-03-01';	
-				  $end = date( 'Y' ).'-06-'.date('d');	
-			  }
-			  
-			  if( $trimestre == 3 ){			
-				  $begind = date( 'Y' ).'-06-01';	
-				  $end = date( 'Y' ).'-09-'.date('d');	
-			  }
-			  
-			  if( $trimestre == 4 ){			
-				  $begind = date( 'Y' ).'-09-01';	
-				  $end = date( 'Y' ).'-12-'.date('d');	
-			  }
-			  
-				  
-			  $this->db->where( array( 'work_order.creation_date >= ' => $begind, 'work_order.creation_date <=' =>  $end ) ); 
-		  }
-		  
-		  
-		  
-		  
-		  
-		  
-	  if( $filter['query']['periodo'] == 3 )
-	  
-		  $this->db->where( array( 'work_order.creation_date >= ' => $anio,  'work_order.creation_date <=' => date( 'Y-m-d' ) ) ); 
 
-		if( $filter['query']['periodo'] == 4 )
-		{
-			$from = $this->custom_period_from;
-			$to = $this->custom_period_to;
-			if ( ( $from === FALSE ) || ( $to === FALSE ) )
-			{
-				$from = date('Y-m-d');
-				$to = $from;
-			}
-			$this->db->where( array(
-				'work_order.creation_date >= ' => $from . ' 00:00:00',
-				'work_order.creation_date <=' => $to . ' 23:59:59') );
+		if( isset( $filter['query']['ramo'] ) and !empty( $filter['query']['ramo'] ) ){
+		  $this->db->where( 'work_order.product_group_id', $filter['query']['ramo'] ); 
 		}
-	  }				
-  }
+	}
 	
 	$query = $this->db->get(); 
-  	
+
 	if ($query->num_rows() == 0) return 0;	
 	
 	$count = 0;
