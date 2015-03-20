467bc32b0fa874557fcfc6d78c62a566be50562e
flai <flai1789@gmail.com>
2015-03-20 11:42:51 +0100

    Show policy number on Ver OT page

--- application/modules/ot/views/update_view.php
+++ application/modules/ot/views/update_view.php
@@ -335,9 +335,10 @@ else
                     </div>
                   </div>
 
-<?php if (($function == 'update') && (
+<?php if (($function != 'update') ||
+		(($function == 'update') && (
 		($is_nuevo_negocio && in_array($data['status_id'], array(7, 4, 10))) ||
-		(!$is_nuevo_negocio)) ) :?>
+		(!$is_nuevo_negocio)) )) :?>
                   <div class="control-group poliza">
                     <label class="control-label text-error" for="inputError">Poliza</label>
                     <div class="controls">
