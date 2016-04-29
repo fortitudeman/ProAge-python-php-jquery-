9576f107606aed8c01bca699b3bb7fad76036f43
flai <flai1789@gmail.com>
2016-04-29 12:23:31 +0200

    Use CI Email class instead of the PHP mail() function

---
+++ application/config/email.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array(
+// 1. With the mail protocol:
+	'protocol' => 'mail',
+// 2. With SMTP (Gmail):
+	'protocol' => 'smtp',
+	'smtp_host' => 'ssl://smtp.gmail.com',
+	'smtp_user' => 'admin.proages@isinet.ws',
+	'smtp_pass' => 'AH@YQLxj',
+	'smtp_port' => '465',
+	'newline' => "\r\n",
+);
+ 
+/* End of file email.php */
+/* Location: ./application/config/email.php */
