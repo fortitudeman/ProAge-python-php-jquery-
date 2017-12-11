68b6ddaaa557b23339f643b391525eb7f43f67d7
Gustavo Sanchez <gustavosan_@hotmail.com>
2017-12-11 14:08:34 -0600

    PROAGES-76 Change product names as ortography

--- isinet_proages_update_2017_12_11.sql
+++ isinet_proages_update_2017_12_11.sql
@@ -1,3 +1,5 @@
 UPDATE work_order_status set `name`='Excedida' where `name`='Excedido';
 UPDATE work_order_status set `name`='Aceptada' where `name`='Aceptado';
 UPDATE work_order_status set `name`='Rechazada' where `name`='Rechazado';
+UPDATE products set `name`='Visión Plus' where `name`='Vision Plus';
+UPDATE products set `name`='Vida Inversión' WHERE id=5;
