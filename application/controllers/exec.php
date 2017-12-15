e342de274877095fe1095835c8091ceadb9bbdb5
Gustavo Sanchez <gustavosan_@hotmail.com>
2017-12-15 08:33:45 -0600

    PROAGES-79 Revision in Negocios PAI calc, change .gitignore

--- application/controllers/exec.php
+++ application/controllers/exec.php
@@ -33,7 +33,7 @@ class Exec extends CI_Controller {
 	}
 
 	public function repairNegociosPai(){
-		$query = $this->db->query('select * from payments where (select count(*) from policy_negocio_pai where policy_number = payments.policy_number) = 0 and year_prime = 1 and valid_for_report = 1 ORDER BY policy_number asc, payment_date asc');
+		$query = $this->db->query('select * from payments where (select count(*) from policy_negocio_pai where policy_number = payments.policy_number) = 0 and year_prime = 1 and valid_for_report = 1 and year(payment_date) = 2017 ORDER BY policy_number asc, payment_date asc');
 		$payments = $query->result_array();
 		$policies = array();
 		foreach ($payments as $i => $payment) {
@@ -49,10 +49,11 @@ class Exec extends CI_Controller {
 				if($flag && $total > 12000){
 					$payment_date = $payment["payment_date"];
 					$flag = false;
-					echo $policy_id." $payment_date<br>";
+					echo $policy_id." $payment_date";
 					$count++;
 				}
 			 }
+			 if(!$flag) echo " $total<br>";
 		}
 		echo $count. " " . count($policies);	
 	}
