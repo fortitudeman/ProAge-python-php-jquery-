e6c1e94b31a08c16357eaacb3508ac70f34cddf7
Gustavo Sanchez <gustavosan_@hotmail.com>
2017-12-18 11:11:54 -0600

    PROAGES-76 Reorder code of the model

--- application/modules/ot/models/work_order.php
+++ application/modules/ot/models/work_order.php
@@ -1036,6 +1036,39 @@ class Work_order extends CI_Model{
 		return $query->result_array();
    }
 
+   public function getWorkOrdersGroupByAgents($filter, $orderby){
+   		//Configuration agent group
+		$args = array(
+			"select" => "users.company_name, users.name, users.lastnames, policies_vs_users.percentage, agents.id",
+			"sum" => "policies.prima",
+			"by" => "users.company_name, agents.id, policies_vs_users.percentage",
+			"order" => "$orderby desc",
+		);
+		return $this->getWorkOrdersGroupBy($filter, $args);
+   }
+
+   public function getWorkOrdersGroupByStatus($filter){
+   		//Configuration status group
+		$args = array(
+			"select" => "work_order_status.name status",
+			"sum" => "policies.prima",
+			"by" => "work_order_status.name",
+			"order" => "conteo desc",
+		);
+		return $this->getWorkOrdersGroupBy($filter, $args);
+   }
+
+   public function getWorkOrdersGroupByProducts($filter){
+		//Configuration products group
+		$args = array(
+			"select" => "products.name producto, products.id",
+			"sum" => "policies.prima",
+			"by" => "products.id",
+			"order" => "conteo desc",
+		);
+		return $this->getWorkOrdersGroupBy($filter, $args);
+   }
+
    public function getWorkOrdersGroupByGeneracion($filter){
    		//work_order_status = Pagadas
    		//$filter["where"] = array("work_order_status.id" => 4);
