af8428bd453d68027a7eef3ada904a7df82e02e4
Lázaro Hernández <lazaro.9318@gmail.com>
2018-01-22 12:39:41 -0600

    PROAGES-86 Add - implement new process with JavaScript (jQuery) for modular programming to improve the execution time and better perfomance. Improve the delete function and implement Sweet Alert.

--- application/modules/ot/assets/scripts/import.js
+++ application/modules/ot/assets/scripts/import.js
@@ -1,146 +1,90 @@
-// JavaScript Document
-/*
-
-  Author        Ulises Rodríguez
-  Site:         http://www.ulisesrodriguez.com
-  Twitter:      https://twitter.com/#!/isc_ulises
-  Facebook:     http://www.facebook.com/ISC.Ulises
-  Github:       https://github.com/ulisesrodriguez
-  Email:        ing.ulisesrodriguez@gmail.com
-  Skype:        systemonlinesoftware
-  Location:     Guadalajara Jalisco Mexíco
-
-
-  */
-  $( document ).ready( function(){
-
-
-  	$( "#dialog-form" ).dialog({
-  		autoOpen: false,
-  		height: 600,
-  		width: 800,
-  		modal: true,
-  		buttons: {
-  			Cerrar: function() {
-  				$( this ).dialog( "close" );
-  				$.ajax({
-
-  					url:  Config.base_url()+'ot/getSelectAgents.html',
-  					type: "POST",
-  					cache: false,
-  					async: false,
-  					success: function(data){
-  						var option = $( '#control' ).val();
-
-  						option = option.split('-');
-
-  						$( '.options-'+option[1] ).html(data);
-  					}
-  				});
-  			}
-  		}
-  	});
-
-  	$( '.create-user' ).bind( 'click', function(){
-
-  		$( "#dialog-form" ).dialog( "open" );
-
-  		$( '#control' ).val( this.id );
-
-  	});
-
- /*
-$( ".create-user" )
-  .button()
-  .click(function() {
-	$( "#dialog-form" ).dialog( "open" );
-});*/
-
-$("#delete-submit").on( "click", function( event ) {
-
-	var product = $("#product-type-delete").val();
-	var selectedMonth = $("#month-delete").val();
-	var selectedYear = $("#year-delete").val();
-	if ( (product.length == 0) || (product < 1) || (product > 3) ) {
-		swal('El tipo de producto es invalido (seleccione un valor).');
-		return false;
-	}
-	if ( (selectedMonth < 1) || (selectedMonth > 12) ||
-		( selectedYear < 1900 ) || ( selectedYear > 2100 ) ) {
-		swal('El mes - año estan invalidos.');
-	return false;
-}
-
-var tipoProduct = '';
-$("#product-type-delete option:selected").each(function () {
-	tipoProduct = ' (tipo de producto : ' + $(this).text() + ')';
-	return false;
-});
-
-var confirmMessage = '¿Está seguro que desea borrar todos los datos de pago del mes '
-+ selectedMonth + '/' + selectedYear + tipoProduct + ' ?';
-
-if ( confirm( confirmMessage ) ) {
-	url = Config.base_url() + 'ot/delete_payments.html';
-	$.ajax({
-		url: url,
-		type: 'POST',
-		data: $( "#import-delete").serialize(),
-		dataType : 'json',
-		beforeSend: function(){
-			$( "#delete-submit").hide();
+(function(){
+	var imports = {
+		init: function(){
+			this.cacheDom();
+			this.binder();
 		},
-		success: function(response){
-			$( "#delete-submit").show();
-			switch (response) {
-				case '-1':
-				swal ('No se pudo borrar los pagos. Informe a su administrador.');
-				break;
-				case '-2':
-				swal ('Ocurrio un error, no se pudo borrar los pagos, consulte a su administrador.');
-				break;
-				case '0':
-				swal ('No hay pagos para el mes - año ' + selectedMonth + '/' + selectedYear  + tipoProduct + '.');
-				break;
-				default:
-				swal ('Se pudo borrar los pagos del mes - año ' + selectedMonth + '/' + selectedYear  + tipoProduct + ' correctamente.');
-				break;
-			}
-		}
-	});
-}
-return false;
-});
-
-$("#woAsPAI").click(function(){
-	if($(this).prop('checked')){
-		$(".wo","#woListPAI").prop( "checked", true );
-	}else{
-		$(".wo","#woListPAI").removeAttr('checked');
-
-	}
-});
-
-
-$('#importPayment').on('submit', function(event){
-	event.preventDefault();
-	$.ajax({
-		url:Config.base_url() +"ot/import_payments",
-		method:"POST",
-		data:new FormData(this),
-		contentType:false,
-		cache:false,
-		processData:false,
-		beforeSend:function(){
-			$('#importBtn').html('Importando...');
+		cacheDom: function() {
+			this.$formDelete = $("#import-delete");
+			this.$product = $("#product-type-delete");
+			this.$selectedMonth =this.$formDelete.find("#month-delete");
+			this.$month = $('#month-delete').find('option:selected').val();
+			this.$selectedYear = this.$formDelete.find("#year-delete");
+			this.$btnDelete = this.$formDelete.find("#delete-submit");
+			this.$importPayment = $('#formfile');
+			this.$btnImport = this.$importPayment.find("#btnImport");
 		},
-		success:function(data)
-		{
-			$('#importPayment')[0].reset();
-			$('#importBtn').attr('disabled', false);
-			$('#importBtn').html('Importar');
-			swal("Se importó de forma exitosa.");
+		binder: function(){
+			this.$btnDelete.on('click',this.deletePayments.bind(this));
+			//this.$importPayment.on('submit',this.importPayments.bind(this));
+		},
+		deletePayments: function(){
+			swal({
+				title: "¿Esta seguro que desea eliminar estos pagos?",
+				text: "Una vez realizada la operación, no se podrá deshacer.",
+				icon: "warning",
+				buttons: true,
+				dangerMode: true,
+			})
+			.then((willDelete) => {
+				if (willDelete) {
+					url = Config.base_url() + 'ot/delete_payments.html';
+					$.ajax({
+						url: url,
+						type: 'POST',
+						data: $( "#import-delete").serialize(),
+						dataType : 'json',
+						beforeSend: function(){
+							$( "#delete-submit").hide();
+						},
+						success: function(response){
+							console.log(this.$month);
+							$( "#delete-submit").show();
+							switch (response) {
+								case '-1':
+								swal ('No se pudo borrar los pagos. Informe a su administrador.');
+								break;
+								case '-2':
+								swal ('Ocurrio un error, no se pudo borrar los pagos, consulte a su administrador.');
+								break;
+								case '0':
+								swal ('No hay pagos para el mes - año - producto seleccionados.');
+								break;
+								default:
+								swal("La operación se ha realizado con exito!", {
+									icon: "success",
+								});
+								break;
+							}
+						}
+					});
+				}
+			});
+			return false;
+		},
+		importPayments: function(){
+			$.ajax({
+				url:Config.base_url() +"ot/import_payments",
+				method:"POST",
+				data:new FormData(this),
+				contentType:false,
+				cache:false,
+				processData:false,
+				beforeSend:function(){
+					this.$btnImport.html('Importando...');
+				},
+				success:function(data)
+				{
+					this.$importPayment.reset();
+					this.$btnImport.attr('disabled', false);
+					this.$btnImport.html('Importar');
+					swal("La importación se ha realizado con exito!", {
+						icon: "success",
+					});
+				}
+			})
+			return false;
 		}
-	})
-});
-});
+	};
+	imports.init();
+})()
