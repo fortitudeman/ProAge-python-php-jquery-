931235db5c79ca510ec50dfdecd5e367bbc7cad1
Jesús Castilla <jesus@isinet.com>
2018-04-23 15:53:42 -0500

    remove --> on header

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -107,7 +107,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWJ55N6"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<!--
 	<?php if(!isset($no_visible_elements) || !$no_visible_elements)	{ ?>
 	<!-- topbar starts -->
 	<div class="navbar">
@@ -209,7 +208,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                         <?php foreach( $roles_vs_access  as $value ): if( in_array( 'Orden de trabajo', $value ) ): ?>
                         <li><a href="<?php echo base_url() ?>ot.html"><i class="icon-tablet"></i><span class="hidden-tablet">Orden trabajo</span></a></li>
                         <?php break; endif; endforeach; ?>
-                    -->
                     <?php if( isset($user) && isset($user['id'])):
                     foreach( $roles_vs_access  as $value ):
                     	if( in_array( 'Operations', $value ) ): ?>
