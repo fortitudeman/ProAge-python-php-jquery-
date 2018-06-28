29b648c41dadde134ef1eac87b2e683582d41620
Lázaro Hernández Cruz <lazaro@isinet.ws>
2018-06-27 23:12:58 -0500

    changes in script for update payments

--- application/updateScript/updatePais.py
+++ application/updateScript/updatePais.py
@@ -11,39 +11,35 @@ db = MySQLdb.connect(host="proages-db.coroolzydzjr.us-east-1.rds.amazonaws.com",
                      cursorclass=MySQLdb.cursors.DictCursor)
 cur = db.cursor()
 cursor = db.cursor()
-
+sql = "SELECT * FROM payments WHERE year_prime = 1 and payment_date between '%s-%s-01' and LAST_DAY('%s-%s-01')"
+update = "UPDATE payments SET pai_business = %s WHERE pay_tbl_id = %s"
 updatePai = dict()
 years = [2014, 2015, 2016, 2017,  2018]
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 try:
     for year in years:
         for month in months:
-            sql = "SELECT * FROM payments WHERE year_prime = 1 and payment_date like '%s-%s-%'"
-            rows = cur.execute(sql,year,month)
+            values = (year, month, year, month)
+            rows = cur.execute(sql, values)
             if  rows > 0:
                 for row in cur:
                     cursor.execute("SELECT SUM(pai_business) as totalPai FROM payments WHERE policy_number = %s", row["policy_number"])
-                    
-                    actualDate = updatePai[row['policy_number']]['payment_date']
-                    
+                    actualDate = 0 if (not updatePai['policy_number']['date']) else updatePai['policy_number']['date'] 
                     if row['policy_number'] in updatePai and compareDates(actualDate, row['payment_date']):
                         
                         updatePai[row['policy_number']]['id'] = row['pay_tbl_id']
                         updatePai[row['policy_number']]['amount'] += row['amount']
                         pai = calculatePai(
                             updatePai['amount'], row['payment_date']) - cursor["totalPai"]
-                        updatePai[row['policy_number']]['pai'] = pai
                     
                     else:
-                    
-                        updatePai[row['policy_number']] = row['policy_number']
                         updatePai[row['policy_number']]['amount'] = row['amount']
                         updatePai[row['policy_number']]['date'] = row['payment_date']
                         pai = calculatePai(
                             updatePai['amount'], row['payment_date']) - cursor["totalPai"]
-                        updatePai[row['policy_number']]['pai'] = pai
-                    
-                    cur.execute("UPDATE payments SET pai_business = %s WHERE pay_tbl_id = %s", pai, updatePai[row['policy_number']]['id'])
+                    valuesUpdate = (pai, updatePai[row['policy_number']]['id'])
+
+                    cur.execute(update, valuesUpdate)
                     cursor.clear()
                 cur.clear()
 finally:
@@ -82,7 +78,9 @@ finally:
         return pai
 
     def compareDates(dateOne, dateTwo):
-        first = datetime.datetime.strptime(dateOne, "%Y-%m-%d")
-        second = datetime.datetime.strptime(dateTwo, "%Y-%m-%d")
-        return True if (first.month == second.month and first.year == second.year) else False
-        
+
+        if  dateOne != 0 and dateTwo != 0:
+            first = datetime.datetime.strptime(dateOne, "%Y-%m-%d")
+            second = datetime.datetime.strptime(dateTwo, "%Y-%m-%d")
+            return True if (first.month == second.month and first.year == second.year) else False
+        return False    
