8474fe775d4fca605cd522f02a475336a785f341
Lázaro Hernández Cruz <lazaro@isinet.ws>
2018-07-05 23:33:31 -0500

    add setup the column pai business to 0.

--- application/updateScript/updatePais.py
+++ application/updateScript/updatePais.py
@@ -51,6 +51,7 @@ years = [2014, 2015, 2016, 2017,  2018]
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 totalRows = 0
 try:
+    cur.execute("UPDATE paymentss SET pai_business = 0")
     for year in years:
         print("Year: %s", year)
         for month in months:
